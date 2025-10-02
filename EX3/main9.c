111dec806dec4bbfeb647f23f1bee4cd418d75cb
Tung-Cho <125737928+Tung-Cho@users.noreply.github.com>
2025-10-02 15:02:48 +0800

    Update main9.c

--- EX3/main9.c
+++ EX3/main9.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main()
+{
+    int x = 0, y = 0, z = 0;
+    scanf("%d %d %d",&x,&y,&z);
+    if(x < 0){
+        int sum = (x*-1)*100 + y*10 + z;
+        printf("%d",sum*-1);
+    }else{
+        printf("%d",x*100 + y*10 + z);
+    }
+    
+    return 0;
+}
