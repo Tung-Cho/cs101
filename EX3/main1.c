3a0f19ef2b3ed19da870f76283cbcbe43924c6c9
Tung-Cho <125737928+Tung-Cho@users.noreply.github.com>
2025-10-02 14:37:15 +0800

    Update main1.c

--- EX3/main1.c
+++ EX3/main1.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main()
+{
+    for(int i = 1; i <= 6; ++i){
+        for(int j = 6; j >= 1; --j){
+            if(j-i > 0) printf(" ");
+            else if(j-i <= 0)printf("%d " ,i);
+        }
+        printf("\n");
+    }
+}
