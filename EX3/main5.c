aaf0d3dc6952ebec48906ea80a36b4fde5378d6c
Tung-Cho <125737928+Tung-Cho@users.noreply.github.com>
2025-10-02 14:49:53 +0800

    Update main5.c

--- EX3/main5.c
+++ EX3/main5.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main()
+{
+    int i = 0 ;
+    scanf("%d",&i);
+    if(i%400 == 0 || (i%4 == 0 && i%100 != 0)){
+        printf("閏年");
+    }else{
+        printf("不是閏年");
+    }
+
+    return 0;
+}
