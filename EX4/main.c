70e71c709b4a743db02ccce6455b32b07b40f159
Tung-Cho <125737928+Tung-Cho@users.noreply.github.com>
2025-10-09 16:06:10 +0800

    Update main.c

--- EX4/main.c
+++ EX4/main.c
@@ -1,4 +1,37 @@
- fp = fopen("cj.bin","wb+");
+#include <stdio.h>
+
+int main()
+{
+    FILE*fp;
+    int a[] = {0,1,2};
+    char b[] = "ABC";
+    float c[] = {1.1,1.2,1.3};
+    int ra[3];
+    char rb[3];
+    float rc[3];
+    
+    
+    
+    fp = fopen("cj.bin","wb+");
     fwrite(a,sizeof(a),1,fp);
+    fwrite(b,sizeof(b),1,fp);
+    fwrite(c,sizeof(c),1,fp);
     fseek(fp,0,SEEK_SET);
     fread(ra,sizeof(ra),1,fp);
+    
+    fseek(fp,sizeof(a),SEEK_SET);
+    fread(rb,sizeof(rb),1,fp);
+   
+    fseek(fp,sizeof(a)+sizeof(b),SEEK_SET);
+    fread(rc,sizeof(rc),1,fp);
+    
+    for(int i = 0; i < 3; ++i){
+        printf("%d",ra[i]);
+    }
+    for(int i = 0; i < 3; ++i){
+        printf("%c",rb[i]);
+    }
+    for(int i = 0; i < 3; ++i){
+        printf("%f",rc[i]);
+    }
+}
